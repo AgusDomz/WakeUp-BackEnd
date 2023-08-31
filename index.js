e9c7c0a9426922d43a970149a833a79d6d0fbc2f
Agustín Dominguez <131421006+AgusDomz@users.noreply.github.com>
2023-08-31 16:30:51 -0300

    correction scaffolding

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // DOTENV REQUIRE
 require("dotenv").config();
 
+// MORGAN REQUIRE
+const morgan = require("morgan");
+
+
 // EXPRESS REQUIRE
 const express = require("express");
 
 // DATABASE CONNECT
-const connectDB = require("./config/mongoDB");
+const connectDB = require("./src/config/mongoDB");
 connectDB();
 
 // OUR APP USE EXPRESS
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 // PORT
 const port = process.env.PORT || 3000;
